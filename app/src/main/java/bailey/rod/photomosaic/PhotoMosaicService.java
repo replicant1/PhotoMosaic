6d776c77ecca99ae1e50b29d465b64cd237be0b3
rodbailey <rodney.p.bailey@gmail.com>
2016-07-17 01:32:48 +1000

    First cut at tile averaging is working OK

--- app/src/main/java/bailey/rod/photomosaic/PhotoMosaicService.java
+++ app/src/main/java/bailey/rod/photomosaic/PhotoMosaicService.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -28,7 +29,7 @@ import static timber.log.Timber.i;
 /**
  * A service that applies a "Mosaic" effect to a given image file. The image file is modified "in place" in an
  * asynchronous manner. The service broadcasts updates as it proceeds
- * <p/>
+ * <p>
  * To trigger the service, broadcast an Intent with the following data:
  * <ul>
  * <li> EXTERNAL_PATH_URI - Path to the raw image file to be mosaic'd. It should be somewhere where this service
@@ -92,10 +93,58 @@ public class PhotoMosaicService extends IntentService {
         try {
             // TODO: Maybe do something more efficient based on loading some attributes of image?
             Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
-            
-        }
-        catch (IOException iox) {
+            int TILE_WIDTH_PX = 32;
+            int TILE_HEIGHT_PX = 32;
+
+            int pixel;
+            int redComponent;
+            int blueComponent;
+            int greenComponent;
+            int alphaComponent;
+            int pixelCount;
+
+            for (int x = 0; x < bitmap.getWidth(); x += 32) {
+                for (int y = 0; y < bitmap.getHeight(); y += 32) {
+
+                    Timber.d("Processing tile [%d, %d]", x, y);
+
+                    redComponent = 0;
+                    blueComponent = 0;
+                    greenComponent = 0;
+                    alphaComponent = 0;
+
+                    pixelCount = 0;
+
+                    for (int x1 = x; (x1 < (x + 31)) && (x1 < bitmap.getWidth()); x1++) {
+                        for (int y1 = y; (y1 < (y + 31)) && (y1 < bitmap.getHeight()); y1++) {
+                            // Find the average color of the tile whose top left corner
+                            // is at  [x,y]
+                            pixel = bitmap.getPixel(x, y);
+                            pixelCount++;
+
+                            redComponent += Color.red(pixel);
+                            blueComponent += Color.blue(pixel);
+                            greenComponent += Color.green(pixel);
+                            alphaComponent += Color.alpha(pixel);
+                        }
+                    }
+
+
+                    int averageRed = redComponent / pixelCount;
+                    int averageBlue = blueComponent / pixelCount;
+                    int averageGreen = greenComponent / pixelCount;
+                    int averageAlpha = alphaComponent / pixelCount;
+
+                    // Broadcast a new result - another tile has been averaged.
+                    Timber.d("Tile [%d, %d] has average color of (R:%d G:%d B:%d A:%d)", x, y, averageRed, averageGreen,
+                             averageBlue, averageAlpha);
+                } // for y
+            } // for x
+
+
+        } catch (IOException iox) {
             Timber.i(iox, "Failed to get bitmap for processing");
         }
     }
+
 }
